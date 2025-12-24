7e7e0bc9b7bcba90686d801f74f01bceece36046
KHK <trafalgalow32@gmail.com>
2025-12-24 10:43:20 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+  public static void main(String [] args){
+     // 하고싶은 코드 작성
+     int kor=90;
+     int math=70;
+
+     int tot = kor + math;
+     double avg = tot / 2.0;
+
+     System.out.println("총점 : "+tot);
+
+  }
+}
